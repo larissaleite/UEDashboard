aafe795aaa1a45343a8452ed293737b149e2191b
larissaleite <larissa.leite@gmail.com>
2015-06-06 02:54:13 -0300

    Filters added

--- UEDashboard/app/routes.py
+++ UEDashboard/app/routes.py
@@ -27,6 +27,7 @@ def get_all_users():
 
 	for row in result:
 		users.append(str(row["developer"]))
+	result.close()
 	return users
 
 def get_months_ordered():
@@ -36,6 +37,7 @@ def get_months_ordered():
 	
 	for row in result:
 		months.append(int(row["month"]))
+	result.close()
 	return months
 
 def get_months_categories():
@@ -55,7 +57,7 @@ def get_commits_user():
 		result = db.get_engine(app, 'db_commits').execute(sql)
 		n = str(result.fetchone()[0])
 		commits_by_month.append(int(n))
-
+		result.close()
 	commits.append({ 'data' : commits_by_month, 'name' : str(user) })
 
 	return commits
@@ -66,6 +68,7 @@ def get_events_user():
 	result = db.get_engine(app, 'db_events').execute(sql)
 	for row in result:
 		events.append({ 'date' : row["date"], 'commit_message' : row["message"], 'message' : row["msg"], 'type' : row["type"], 'id' : row["id"], 'id_commit' : row["id_commit"] })
+	result.close()
 	return events
 
 def get_events_team():
@@ -77,6 +80,7 @@ def get_events_team():
 		if row["type"] == "Time Between Commits":
 			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
 		events.append({ 'date' : row["date"], 'commit_message' : row["message"], 'message' : message, 'type' : row["type"], 'id' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+	result.close()
 	return events
 
 #TEMPLATE FILES
@@ -100,10 +104,12 @@ def show_team_commits():
 		if developer != user:
 			for month in get_months_ordered():
 				sql = text("select count(*) from commit where author='"+developer+"' and MONTH(date)="+str(month))
-				result = db.get_engine(app, 'db_commits').execute(sql)
-				n = str(result.fetchone()[0])
+				res = db.get_engine(app, 'db_commits').execute(sql)
+				n = str(res.fetchone()[0])
 				commits_by_month.append(int(n))
+				res.close()
 			commits.append({ 'data' : commits_by_month, 'name' : developer })
+	result.close()
 
 	months_categories = get_months_categories()
 	events = get_events_team()
@@ -117,10 +123,10 @@ def get_all_commits():
 	commits = []
 	for row in result:
 		commits.append({ 'developer' : row["developer"], 'date' : row["date"] })
-
+	result.close()
 	return commits
 
-@app.route('/api/commits_detail', methods = ['POST'])
+@app.route('/api/commit/detail', methods = ['POST'])
 def get_detail_commit():
 	commits_detail = []
 
@@ -131,14 +137,164 @@ def get_detail_commit():
   	modifications = []
   	for row in result:
   		modifications.append({ 'file' : row["file"], 'type' : row["type"] })
+  	result.close()
 
   	sql = text("select commit.message, commit.developer, commit.date from commit where id="+id_commit+";")
   	result = db.get_engine(app, 'db_events').execute(sql)
   	commit = result.fetchone()
   	commits_detail.append({ 'message' : commit["message"], 'developer' : commit["developer"], 'date' : json.dumps(commit["date"].isoformat()), 'modifications' : modifications })
-
+  	result.close()
   	return jsonify(commits_detail=commits_detail)
-  	 
+
+@app.route('/api/events/developer', methods = ['GET'])
+def get_events_by_developer():
+	sql = text("select commit.id as id_commit, commit.date, commit.developer, event.message as msg, event.type, event.id from event inner join commit on event.id_commit = commit.id order by developer")
+	result = db.get_engine(app, 'db_events').execute(sql)
+	#dev = result.fetchone()["developer"]
+	dev = ""
+	first = True
+	developers = []
+	dev_array = {}
+	for row in result:
+		if first is True:
+			dev = row["developer"]
+			first = False
+			
+		if row["developer"] != dev:
+			dev_array[dev] = developers
+			dev = row["developer"]
+			developers = []
+
+		message = row["msg"]
+		if row["type"] == "Time Between Commits":
+			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
+		developers.append({ 'date' : json.dumps(row["date"].isoformat()), 'message' : message, 'type' : row["type"], 'id_event' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+
+	result.close()
+
+	#add last developer
+	dev_array[dev] = developers
+	return jsonify(events=dev_array)
+
+@app.route('/api/events/date', methods = ['GET'])
+def get_events_by_date():
+	sql = text("select commit.id as id_commit, commit.date, commit.developer, event.message as msg, event.type, event.id from event inner join commit on event.id_commit = commit.id order by date")
+	result = db.get_engine(app, 'db_events').execute(sql)
+	#date = result.fetchone()["date"]
+	date = ""
+	first = True
+	dates = []
+	date_array = {}
+	for row in result:
+		if first is True:
+			date = row["date"]
+			first = False
+
+		if row["date"] != date:
+			date_array[json.dumps(date.isoformat())] = dates
+			date = row["date"]
+			dates = []
+
+		message = row["msg"]
+		if row["type"] == "Time Between Commits":
+			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
+		dates.append({ 'message' : message, 'type' : row["type"], 'id_event' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+
+	result.close()
+
+	#add last date
+	date_array[json.dumps(date.isoformat())] = dates
+	return jsonify(events=date_array)
+
+@app.route('/api/events/date/developer', methods = ['GET'])
+def get_events_by_date_developer():
+	sql = text("select commit.id as id_commit, commit.date, commit.developer, event.message as msg, event.type, event.id from event inner join commit on event.id_commit = commit.id where commit.developer='"+user+"' order by date")
+	result = db.get_engine(app, 'db_events').execute(sql)
+	#date = result.fetchone()["date"]
+	date = ""
+	first = True
+	dates = []
+	date_array = {}
+	for row in result:
+		if first is True:
+			date = row["date"]
+			first = False
+
+		if row["date"] != date:
+			date_array[json.dumps(date.isoformat())] = dates
+			date = row["date"]
+			dates = []
+
+		message = row["msg"]
+		if row["type"] == "Time Between Commits":
+			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
+		dates.append({ 'message' : message, 'type' : row["type"], 'id_event' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+
+	result.close()
+
+	#add last date
+	date_array[json.dumps(date.isoformat())] = dates
+	return jsonify(events=date_array)
+
+@app.route('/api/events/commit', methods = ['GET'])
+def get_events_by_commit():
+	sql = text("select commit.id as id_commit, commit.date, commit.developer, commit.message, event.message as msg, event.type, event.id from event inner join commit on event.id_commit = commit.id order by date")
+	result = db.get_engine(app, 'db_events').execute(sql)
+	#id_commit = result.fetchone()["id_commit"]
+	id_commit = ""
+	first = True
+	commits = []
+	commits_array = {}
+	for row in result:
+		if first is True:
+			id_commit = row["id_commit"]
+			first = False
+
+		if row["id_commit"] != id_commit:
+			commits_array[id_commit] = commits
+			id_commit = row["id_commit"]
+			commits = []
+
+		message = row["msg"]
+		if row["type"] == "Time Between Commits":
+			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
+		commits.append({ 'date' : json.dumps(row["date"].isoformat()), 'message' : message, 'type' : row["type"], 'id_event' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+
+	result.close()
+
+	#add last commit
+	commits_array[id_commit] = commits
+	return jsonify(events=commits_array)
+
+@app.route('/api/events/commit/developer', methods = ['GET'])
+def get_events_by_commit_developer():
+	sql = text("select event.id_commit, commit.date, commit.developer, commit.message, event.message as msg, event.type, event.id from event inner join commit on event.id_commit = commit.id where commit.developer='"+user+"' order by date")
+	result = db.get_engine(app, 'db_events').execute(sql)
+	#id_commit = result.fetchone()["id_commit"]
+	id_commit = ""
+	first = True
+	commits = []
+	commits_array = {}
+	for row in result:
+		if first is True:
+			id_commit = row["id_commit"]
+			first = False
+
+		if row["id_commit"] != id_commit:
+			commits_array[id_commit] = commits
+			id_commit = row["id_commit"]
+			commits = []
+
+		message = row["msg"]
+		if row["type"] == "Time Between Commits":
+			message = row["developer"] + " hadn't "+ message[11:-1] + "s"
+		commits.append({ 'date' : json.dumps(row["date"].isoformat()), 'message' : message, 'type' : row["type"], 'id_event' : row["id"], 'id_commit' : row["id_commit"], 'developer' : row["developer"] })
+
+	result.close()
+
+	#add last commit
+	commits_array[id_commit] = commits
+	return jsonify(events=commits_array)
 
 if __name__ == '__main__':
 	app.secret_key = 'secret key'
